11b318e50f61b5309e31c2fa3752de496adadf0d
AlexeyPupyshev <bankir800@gmail.com>
2021-07-27 16:05:06 +0300

    Задания с 4 по 6 подсмотрены в интернете

---
+++ Practical Tasks №8/Practical Task №8-2.py
@@ -0,0 +1,14 @@
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f'На ноль делить нельзя')
+
+print(DivisionByNull.divide_by_null(15, 3))
+print(DivisionByNull.divide_by_null(15, 0))
